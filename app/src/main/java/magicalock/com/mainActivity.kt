3c8ea2e2999c3f3aeafd1cef898fd72e9c28cf4e
Incod3r <yohanesandrew483@gmail.com>
2019-02-06 10:12:41 +0700

    Initial commit

---
+++ app/src/main/java/magicalock/com/mainActivity.kt
@@ -0,0 +1,13 @@
+package magicalock.com
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class mainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+    }
+}
